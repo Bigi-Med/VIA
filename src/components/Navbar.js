50b5f261f9a3bdc0ce0aa1cd63fb555bfe935452
Bigi-Med <medbigi2000@gmail.com>
2023-12-17 20:12:02 +0100

    MBI: adapting navbar to screen size (hamburger + remove logo)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './Navbar.css'
 function Navbar (){
+    
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
         <div className="NavBar">
             <div className='logo'>
                 <img  className = 'logoImg' src ='via-background.png'></img>
                 <h1 className='companyName'>VentureInAi</h1>
             </div>
+
             <div className='NavigationButtons'>
                 <div className='button'>ABOUT</div>
                 <div className='button'>Services</div>
@@ -13,6 +21,13 @@ function Navbar (){
                 <div className='button'>Billing</div>
                 <div className='button'>Contact us</div>
             </div>
+
+            <div className='hamburger' onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        
         </div>
     )
 }
