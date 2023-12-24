77c9a5341ac91ce164d26b85c3e17b5687749eeb
Bigi-Med <medbigi2000@gmail.com>
2023-12-24 13:24:20 +0100

    MBI: add about us section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Services from './components/Services';
 import Servicesheader from './components/Serviceheader'
-import ServiceList from './components/ServiceList';
+import About from './components/About';
 function App() {
   return (
     <div className='container'>
@@ -11,7 +11,10 @@ function App() {
       <div className='serviceHeaders'>
           <Servicesheader body='AI assitant available 24/7 // Impress your leads with immediate responses // Match your clients preferences with available properties // Send personnalized follow up emails and SMS // Book showings for you based on available properties // Keep in touch with already acquired clients to expand your network'/>
       </div>
+      <div className='divider'></div>
+      <About></About>
     </div>
+    
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import './About.css'
+
+function About(){
+    return (
+        <div className='about-container'>
+            <div className='about-title-container'>
+                <h2 className='about-title'>About Us</h2>
+            </div>
+        </div>
+    )
+}
+export default About
